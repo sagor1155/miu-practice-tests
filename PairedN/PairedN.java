52cb80936d7f9c55343c6e32e0f078f2f9197a02
Fuad <fuadmail@gmail.com>
2022-04-15 03:36:52 +0600

    Paired N

---
+++ PairedN/PairedN.java
@@ -0,0 +1,27 @@
+public class PairedN {
+    public static void main(String[] args) {
+        System.out.println(isPairedN(new int[]{1,4,1,4,5,6}, 5));
+        System.out.println(isPairedN(new int[]{1,4,1,4,5,6}, 6));
+        System.out.println(isPairedN(new int[]{0,1,2,3,4,5,6,7,8}, 6));
+        System.out.println(isPairedN(new int[]{1,4,1}, 5));
+        System.out.println(isPairedN(new int[]{8,8,8,8,7,7,7}, 15));
+        System.out.println(isPairedN(new int[]{8,-8,8,8,7,7,-7}, -15));
+        System.out.println(isPairedN(new int[]{3}, 3));
+        System.out.println(isPairedN(new int[]{}, 0));
+    }
+
+    public static int isPairedN(int[] nums, int n) {
+        if (nums.length < 2 || n > ((nums.length-1) + (nums.length - 2))) {
+            return 0;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            for(int j=0; j < nums.length; j++) {
+                if (j == i) continue;
+                if (nums[i] + nums[j] == n && i + j == n) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+}
