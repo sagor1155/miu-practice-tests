41cba9f4cd7c235fd7a38ed466c6ca9f91a76854
Fuad <fuadmail@gmail.com>
2022-04-27 07:24:29 -0500

    PairedN logic update

--- PairedN/PairedN.java
+++ PairedN/PairedN.java
@@ -15,8 +15,7 @@ public class PairedN {
             return 0;
         }
         for(int i = 0; i < nums.length; i++) {
-            for(int j=0; j < nums.length; j++) {
-                if (j == i) continue;
+            for(int j= i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == n && i + j == n) {
                     return 1;
                 }
