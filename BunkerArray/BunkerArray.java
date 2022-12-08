fada8d38dfb1332a506c1c0c6db3cc04fdba7467
sagor <sagor.eee.cuet@gmail.com>
2022-12-09 02:14:44 +0600

    bunker array solved

--- BunkerArray/BunkerArray.java
+++ BunkerArray/BunkerArray.java
@@ -1,16 +1,30 @@
 public class BunkerArray {
     public static void main(String[] args) {
-        System.out.println(isBunker(new int[] {7, 6, 10, 1}));
-        System.out.println(isBunker(new int[] {7, 6, 10}));
-        System.out.println(isBunker(new int[] {6, 10, 1}));
-        System.out.println(isBunker(new int[] {3, 7, 1, 8, 1}));
+        System.out.println(isBunker(new int[] {7, 6, 10, 1}));  // 1
+        System.out.println(isBunker(new int[] {7, 6, 10}));     // 0
+        System.out.println(isBunker(new int[] {6, 10, 1}));     // 0
+        System.out.println(isBunker(new int[] {3, 7, 1, 8, 1}));// 1    
     }
 
     public static int isBunker(int[] a) {
-        return 0;
+        boolean isPrimeNumber = false;
+        boolean isOnePresent = false;
+        for (int i=0; i<a.length; i++) {
+            isPrimeNumber = (isPrime(a[i])==1) ? true : isPrimeNumber;
+            isOnePresent = (a[i]==1) ? true : isOnePresent;
+        }
+        return (isPrimeNumber && isOnePresent) ? 1 : 0;
     }
 
-    private static boolean isPrime(int n) {
-        return false;
+    private static int isPrime(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        for (int i=2; i <= n/2; i++) {
+            if (n%i==0) {
+                return 0;
+            }
+        }
+        return 1;
     }
 }
