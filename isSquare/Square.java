71015e43718891fa32c277470f8f1055c0104a19
epiphanycoder <opuchakraborty@gmail.com>
2022-12-13 22:32:42 +0600

    #resolve of isSquare

--- isSquare/Square.java
+++ isSquare/Square.java
@@ -5,9 +5,18 @@ public class Square {
         System.out.println(isSquare(-4));
         System.out.println(isSquare(8));
         System.out.println(isSquare(0));
+        System.out.println(isSquare(225));
     }
 
     public static int isSquare(int number) {
+        int limit = number / 2;
+        int num = 0;
+        while (num <= limit) {
+            if(num*num == number) {
+                return 1;
+            }
+            num++;
+        }
         return 0;
     }
 }
