f8c690d449963f0801347d6b70fab58669bdfd13
sagor <sagor.eee.cuet@gmail.com>
2022-12-13 23:46:50 +0600

    good spread solved

--- GoodSpread/GoodSpread.java
+++ GoodSpread/GoodSpread.java
@@ -5,6 +5,17 @@ public class GoodSpread {
     }
 
     public static int isGoodSpread(int[] a) {
-        return 0;
+        for (int i=0; i<a.length; i++) {
+            int count = 1;
+            for (int j=i+1; j<a.length; j++) {
+                if (a[i]==a[j]) {
+                    count++;
+                }
+            }
+            if (count > 3) {
+                return 0;
+            }
+        }
+        return 1;
     }
 }
