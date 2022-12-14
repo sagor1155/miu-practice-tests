f9e01de7ed8d2afe1fdb195c66855d44918baa5c
sagor <sagor.eee.cuet@gmail.com>
2022-12-15 01:01:51 +0600

    guthrie sequence solved

--- GuthrieSequence/GuthrieSequence.java
+++ GuthrieSequence/GuthrieSequence.java
@@ -5,9 +5,26 @@ public class GuthrieSequence {
         System.out.println(isGuthrie(new int[]{8,17,4,1}));
         System.out.println(isGuthrie(new int[]{8,4,1}));
         System.out.println(isGuthrie(new int[]{8,4,2}));
+        System.out.println(isGuthrie(new int[]{16,8,4,2,1}));
     }
 
     static int isGuthrie(int[] a) {
-        return 0;
+        if (a.length < 2) {
+            return 0;
+        }
+        int n = a[0];
+        int idx = 0;
+        while (n != 1) {
+            if (n%2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            idx++;
+            if (idx >= a.length || a[idx]!=n) {
+                return 0;
+            }
+        }
+        return 1;
     }
 }
