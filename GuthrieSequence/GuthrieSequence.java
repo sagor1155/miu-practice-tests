39feb3599803ffd522a61d341e16fb71550f8f51
epiphanycoder <opuchakraborty@gmail.com>
2022-12-16 21:54:54 +0600

    #problem solution of isDivisible

--- GuthrieSequence/GuthrieSequence.java
+++ GuthrieSequence/GuthrieSequence.java
@@ -8,6 +8,17 @@ public class GuthrieSequence {
     }
 
     static int isGuthrie(int[] a) {
+        if(a[a.length-1] != 1) {
+            return 0;
+        }
+
+        for(int i = 0; i < a.length - 1; i++) {
+            if(a[i] % 2 == 0) {
+                if(a[i+1] != a[i] / 2) return 0;
+            } else {
+                if(a[i+1] != 3 * a[i] + 1) return 0;
+            }
+        }
         return 0;
     }
 }
