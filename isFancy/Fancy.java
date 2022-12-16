eaa59f25f8337ea0e5582e650e5bde1f8f447ba8
epiphanycoder <opuchakraborty@gmail.com>
2022-12-16 22:12:45 +0600

    #problem solution of isFancy

--- isFancy/Fancy.java
+++ isFancy/Fancy.java
@@ -8,6 +8,24 @@ public class Fancy {
     }
 
     public static int isFancy(int n) {
+        int prev = 1, beforePrev =1;
+        int cur = 0;
+
+        if (n == 1) {
+            return 1;
+        }
+        if( n <= 0) {
+            return 0;
+        }
+        for(int i = 1; i < n/2; i++) {
+            cur = 3*prev + 2*beforePrev;
+            if( cur == n) {
+                return 1;
+            } else {
+                beforePrev = prev;
+                prev = cur;
+            }
+        }
         return 0;
     }
 }
