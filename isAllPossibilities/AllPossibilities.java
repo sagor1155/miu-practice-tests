1a6a55e11e4d346180f1e8c4c955fded29bafd0e
epiphanycoder <opuchakraborty@gmail.com>
2022-12-16 11:14:24 +0600

    #problem solution of isAllPossibilities

--- isAllPossibilities/AllPossibilities.java
+++ isAllPossibilities/AllPossibilities.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AllPossibilities {
     public static void main(String[] args) {
         System.out.println(isAllPossibilities(new int[] {1,2,0,3}));
@@ -8,7 +10,38 @@ public class AllPossibilities {
         System.out.println(isAllPossibilities(new int[] {}));
     }
 
+//    public static int isAllPossibilities(int[] nums) {
+//        if(nums.length == 0) {
+//            return 0;
+//        }
+//
+//        Arrays.sort(nums);
+//        for(int i = 1; i < nums.length; i++) {
+//            if(i != nums[i]){
+//                return 0;
+//            }
+//        }
+//        return nums[0] == 0? 1: 0;
+//    }
+
     public static int isAllPossibilities(int[] nums) {
-        return 0;
+        if( nums.length == 0) {
+            return 0;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            boolean hasNum = false;
+            for(int j = 0; j < nums.length; j++) {
+
+                if(i == nums[j]) {
+                    hasNum = true;
+                    break;
+                }
+            }
+            if(!hasNum) {
+                return 0;
+            }
+        }
+        return  1;
     }
 }
